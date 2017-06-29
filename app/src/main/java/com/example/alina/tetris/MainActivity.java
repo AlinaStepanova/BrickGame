e10fe3c5e9f8dbc8e76839a7a6856d33d077edc8
vstelmashchuk <v.stelmashchuk@mobidev.biz>
2017-06-29 20:43:15 +0300

    fixed don't call onDraw in the Pole objects.

--- app/src/main/java/com/example/alina/tetris/MainActivity.java
+++ app/src/main/java/com/example/alina/tetris/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.alina.tetris;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Pole pole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pole = new Pole(this);
-        pole.setBackgroundColor(Color.WHITE);
         setContentView(R.layout.activity_main);
     }
 }
