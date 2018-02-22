27f2b530b7c3e4eabc2fcf79734493de208b494b
Alina <a0031a@meta.ua>
2018-02-22 17:09:19 +0200

    created layout files for new activities

--- app/src/main/java/com/example/alina/tetris/activities/MainActivity.java
+++ app/src/main/java/com/example/alina/tetris/activities/MainActivity.java
@@ -1,9 +1,13 @@
-package com.example.alina.tetris;
+package com.example.alina.tetris.activities;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.alina.tetris.Controller;
+import com.example.alina.tetris.PlayingArea;
+import com.example.alina.tetris.R;
+import com.example.alina.tetris.ScoreArea;
 import com.example.alina.tetris.listeners.OnControllerListener;
 
 public class MainActivity extends AppCompatActivity implements OnControllerListener {
---
+++ app/src/main/java/com/example/alina/tetris/activities/StartActivity.java
@@ -0,0 +1,15 @@
+package com.example.alina.tetris.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alina.tetris.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
