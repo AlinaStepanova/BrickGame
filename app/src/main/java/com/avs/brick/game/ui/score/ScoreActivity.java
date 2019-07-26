17874f1cb61a6d7a128eb8f14938ab9368bbcc75
Alina <stepanovaalina15@gmail.com>
2019-07-26 19:38:20 +0300

    add animation to share icon

--- app/src/main/java/com/avs/brick/game/ui/score/ScoreActivity.java
+++ app/src/main/java/com/avs/brick/game/ui/score/ScoreActivity.java
@@ -1,14 +1,16 @@
 package com.avs.brick.game.ui.score;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.avs.brick.game.R;
 import com.avs.brick.game.data.SharedPreferencesManager;
 import com.avs.brick.game.utils.AnimationUtil;
 
-import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -26,6 +28,9 @@ public class ScoreActivity extends AppCompatActivity {
     @BindView(R.id.llScores)
     LinearLayout scoresLayout;
 
+    @BindView(R.id.ivShare)
+    ImageView shareScore;
+
     private SharedPreferencesManager sharedPreferencesManager;
 
     @Override
@@ -40,6 +45,7 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         scoresLayout.startAnimation(AnimationUtil.getZoomIn(this));
+        shareScore.startAnimation(AnimationUtil.getZoomIn(this));
         firstScore.setText(sharedPreferencesManager.getFirstValue());
         secondScore.setText(sharedPreferencesManager.getSecondValue());
         thirdScore.setText(sharedPreferencesManager.getThirdValue());
